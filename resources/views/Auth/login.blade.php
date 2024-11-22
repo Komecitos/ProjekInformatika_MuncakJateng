d807b78980378f4abd62c8b397c7dbb5f8f639de
Komecitos <irga.prayoga273@gmail.com>
2024-11-23 00:19:16 +0700

    Template

--- resources/views/Auth/login.blade.php
+++ resources/views/Auth/login.blade.php
@@ -26,7 +26,7 @@
 			<div class="row justify-content-center">
 				<div class="col-md-7 col-lg-5">
 					<div class="wrap">
-						<div class="img" style="background-image: url('{{ asset("images/wp1.png") }}');"></div>
+						<div class="img"></div>
 						<div class="login-wrap p-4 p-md-5">
 							<div class="d-flex">
 								<div class="w-100">
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,22 +5,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Welcome Page</title>
-    <link rel="stylesheet" href="{{ asset('css/welcome.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link
         href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap"
         rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/home.css') }}">
     <script src="{{ asset('js/welcome.js') }}"></script>
 
 </head>
 
-<body style="background-image: url('{{ asset("images/wp1.png") }}');">
+<body>
     <!-- Header Section -->
     <header>
-        <div class="logo">
-            <a href="{{route('welcome')}}"><img src="{{asset('images/logo-white.png')}}" alt="Logo"
-                    style="width: 50px; height: auto;"></a>
+        <div class=" logo">
+            <a href="{{route('welcome')}}"><img src="{{asset('images/muncak.png')}}" alt="Logo"></a>
         </div>
         <nav>
             <ul>
@@ -36,7 +35,7 @@
         <div class="container1">
             <div class="welcome">
                 <h1>Muncak jateng</h1>
-                <p style="margin-top: 20px; font-weight: 400;">Selamat datang di platform pendakian kami! Siapkan
+                <p>Selamat datang di platform pendakian kami! Siapkan
                     dirimu untuk
                     mendaki gunung-gunung indah di Jawa Tengah. Temukan
                     informasi, pesan tiket, dan nikmati pengalaman luar biasa di alam terbuka!</p>
@@ -48,22 +47,22 @@
                 <div class="carousel">
                     <div class="carousel-item">
                         <img src="{{asset('images/mounts/Gunung Bismo.jpg')}}" alt="" class="carousel-img">
-                        <h3 style="font-weight: 200; font-size: 20px;">Gunung Bismo <br>
+                        <h3>Gunung Bismo <br>
                             2000 mdpl</h3>
                     </div>
                     <div class="carousel-item">
                         <img src="{{asset('images/mounts/Gunung Prau.jpg')}}" alt="" class="carousel-img">
-                        <h3 style="font-weight: 200; font-size: 20px;">Gunung Prau <br>
+                        <h3>Gunung Prau <br>
                             2000 mdpl</h3>
                     </div>
                     <div class="carousel-item">
                         <img src="{{asset('images/mounts/Gunung Slamet.jpg')}}" alt="" class="carousel-img">
-                        <h3 style="font-weight: 200; font-size: 20px;">Gunung Slamet <br>
+                        <h3>Gunung Slamet <br>
                             2000 mdpl</h3>
                     </div>
                     <div class="carousel-item">
                         <img src="{{asset('images/mounts/Gunung Sumbing.jpg')}}" alt="" class="carousel-img">
-                        <h3 style="font-weight: 200; font-size: 20px;">Gunung Sumbing <br>
+                        <h3">Gunung Sumbing <br>
                             2000 mdpl</h3>
                     </div>
 
